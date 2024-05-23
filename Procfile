2806585f68f617256f81c23df4793f68b92ee93c
Geroimzx <muhayloscherban1@gmail.com>
2024-05-23 16:41:20 +0300

    Heroku support add

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/papershare-1.0.jar
--- src/main/java/com/papershare/papershare/PaperShareApplication.java
+++ src/main/java/com/papershare/papershare/PaperShareApplication.java
@@ -1,8 +1,11 @@
 package com.papershare.papershare;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -13,5 +16,4 @@ public class PaperShareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaperShareApplication.class, args);
 	}
-
 }
